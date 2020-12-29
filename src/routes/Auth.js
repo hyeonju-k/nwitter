ab46ed255477f0d22a2b25ee2cf20678754ac4e2
HyeonJu <hj950000@ajou.ac.kr>
2020-12-29 20:24:10 +0900

    Log In

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,66 @@
-import React from 'react';
+import { authService } from 'myFirebase';
+import React, { useState} from 'react';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
+    const onChange = (event) => {
+        const { target: { name, value } } = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") { 
+            setPassword(value);
+        }
+    };
+    const onSubmit = async(event) => {
+        event.preventDefault();
+        let data;
+        try {
+            if (newAccount) {
+                // create account
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                // log in
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+        } catch (error) { 
+            setError(error.message);
+        }
+    };
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+
+
+    return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <input
+                name="email"
+                type="text"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={onChange}
+            />
+            <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={onChange}
+            />
+                <input type="submit" value={ newAccount? "Create Account" : "Sign In"} />
+        {error}
+            </form>
+            <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</span>
+    <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+    </div>
+    </div>
+    );
+};
 export default Auth;
