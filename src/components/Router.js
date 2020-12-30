7714e8eba3e77dad949e57a2c4f02d4631d80986
HyeonJu <hj950000@ajou.ac.kr>
2020-12-30 14:00:31 +0900

    Authentication

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,20 +2,28 @@ import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "../routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ?
                     <>
                     <Route exact path="/">
                     <Home />
-                    </Route>
+                        </Route>
+                        <Route exact path="/profile">
+                    <Profile />
+                        </Route>
                     </> : (
+                        <>
                         <Route exact path="/">
                             <Auth />
                         </Route>
+                        </>
                     )}
             </Switch>
         </Router>
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from 'myFirebase';
+import { authService, firebaseInstance } from 'myFirebase';
 import React, { useState} from 'react';
 
 const Auth = () => {
@@ -31,6 +31,19 @@ const Auth = () => {
     };
 
     const toggleAccount = () => setNewAccount((prev) => !prev);
+    const onSocialClick = async (event) => {
+        const {
+            target: { name },
+        } = event;
+        let provider;
+        if (name === "google") {
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        } else if (name === "github") { 
+            provider = new firebaseInstance.auth.GithubAuthProvider();
+        }
+        const data = await authService.signInWithPopup(provider);
+        console.log(data);
+    }
 
 
     return (
@@ -57,8 +70,8 @@ const Auth = () => {
             </form>
             <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</span>
     <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+                <button onClick={onSocialClick} name="google">Continue with Google</button>
+                <button onClick={onSocialClick} name="github">Continue with Github</button>
     </div>
     </div>
     );
